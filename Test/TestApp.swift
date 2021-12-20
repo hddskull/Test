046ee5177a3f8c39ca16b100f41670c341b063e3
Max <maxdgladkov@gmail.com>
2021-12-20 17:33:55 +0300

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Max Gladkov on 20.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
