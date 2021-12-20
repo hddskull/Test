9213127cf4c4ceea6898b1582cbcb00b99f8a92c
Max <maxdgladkov@gmail.com>
2021-12-20 17:37:11 +0300

    Change ContentView

--- Test/ContentView.swift
+++ Test/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        NavigationView {
+            Section {
+                Text("Pee-pee")
+                Button("Poo-poo") {
+                    print("Pooped")
+                }
+            }
+            .navigationTitle("Peepee Poopoo time")
             .padding()
+        }
     }
 }
 
